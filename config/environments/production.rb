c09f7794004c199e85309f5b05249f547d436fbb
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-06 00:56:58 +0300

    Add default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,4 +114,4 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
 
-Rails.application.routes.default_url_options = { host: 'https://salu-final-capstone.herokuapp.com' }
+Rails.application.routes.default_url_options = { host: 'https://final-car-rent-api.herokuap' }
