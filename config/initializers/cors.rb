9dab61c3381debe312b5c75d837da82badba75e0
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-06 15:17:46 +0300

    Remove comments

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,8 +8,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
 
   allow do 
-      # origins "http://localhost:3006"
-      # origin "*"
       origins 'https://salu-final-capstone.herokuapp.com'
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
