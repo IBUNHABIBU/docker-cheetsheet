9f67bcf45ea257129afc700fe37a43344f4067cd
IBUNHABIBU <ibunhabib@gmail.com>
2022-08-07 19:37:44 +0300

    Add aws initializer

--- config/initializers/aws.rb
+++
@@ -1,8 +0,0 @@
-require 'aws-sdk-s3'
-
-Aws.config.update({
-  region: ENV['us-east-2'],
-  credentials: Aws::Credentials.new(ENV['access_key_id'], ENV['secret_access_key']),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['rails-api-images'])
