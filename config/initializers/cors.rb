d19502e389b8a3df6c49171a9872682c35779a59
IBUNHABIBU <ibunhabib@gmail.com>
2022-12-16 14:34:56 +0300

    Changing url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,8 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
 
   allow do 
-      # origins 'https://salu-final-capstone.herokuapp.com'
-      origins 'http://localhost:3000'
+      origins 'salum-microverse-final-capstone.netlify.app'
+      # origins 'http://localhost:3000'
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
