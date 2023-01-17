21a2ce9cf9bee6165877d35f69d134ba7e919853
IBUNHABIBU <ibunhabib@gmail.com>
2023-01-17 04:17:18 +0300

    Add asset config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,8 @@ Rails.application.configure do
 
   config.allowed_cors_origins = "*"
 
+  config.assets.compile = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
