697f154436dfdb2b4c450f40fe6a2836ea661268
IBUNHABIBU <ibunhabib@gmail.com>
2023-02-11 02:43:25 +0300

    Add default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,19 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.railway.app",
+    port: 587,
+    domain: "https://web-production-6ac38.up.railway.app/",
+    authentication: "plain",
+    user_name: ENV["RAILWAY_SMTP_USERNAME"],
+    password: ENV["RAILWAY_SMTP_PASSWORD"],
+    enable_starttls_auto: true
+  }
+
+  # Serve static assets in production.
+  config.public_file_server.enabled = true
   # config.public_file_server.enabled = true
   config.hosts << "https://web-production-6ac38.up.railway.app/"
 
