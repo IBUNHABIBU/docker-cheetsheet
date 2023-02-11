1817031a51c0c50d44ed5cf1f6be7f64d5a326fc
IBUNHABIBU <ibunhabib@gmail.com>
2023-02-11 12:19:45 +0300

    Update db in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -131,4 +131,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
 
-Rails.application.routes.default_url_options = { host: 'https://final-car-rent-api.netlify.app/' }
+# Rails.application.routes.default_url_options = { host: 'https://final-car-rent-api.netlify.app/' }
+Rails.application.routes.default_url_options = { host: 'https://web-production-6ac38.up.railway.app/' }
