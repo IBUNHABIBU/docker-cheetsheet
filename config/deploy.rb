fd2b9349ed7b52afa04d142156c82742fa8be57f
IBUNHABIBU <ibunhabib@gmail.com>
2023-02-17 12:08:26 +0300

    Config the deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "car-rent-booking-api"
 set :repo_url, "https://github.com/IBUNHABIBU/car-rent-booking-api.git"
 
 # Deploy to the user's home directory
-set :deploy_to, "/home/deploy/#{fetch :application}"
+set :deploy_to, "/home/deployer/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
@@ -13,6 +13,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 set :keep_releases, 5
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, ENV['BRANCH'] || 'dev'
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
