84e088583bba9c35b25769339ddef46a32465aba
Ibunhabibu <Ibunhabib@gmail.com>
2023-03-12 10:51:15 +0300

    Change origin url to that of netlify production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,8 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
 
   allow do 
-      # origins 'salum-microverse-final-capstone.netlify.app'
-      origins 'http://localhost:3006'
+      origins 'salum-microverse-final-capstone.netlify.app'
+      # origins 'http://localhost:3006'
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
