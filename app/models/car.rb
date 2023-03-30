009b5ba35cbddda9e467b3c4cd18164e72ea99a2
Ibunhabibu <Ibunhabib@gmail.com>
2023-03-30 15:44:39 +0300

    Add image url

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,7 +5,7 @@ class Car < ApplicationRecord
   scope :recent, ->(max_limit = 8) { order('created_at').limit(max_limit) }
 
   def imageUrl
-    image.blob.service_url if image.attached?
-    # Rails.application.routes.url_helpers.url_for(image) if image.attached?
+    # image.blob.service_url if image.attached?
+    Rails.application.routes.url_helpers.url_for(image) if image.attached?
   end
 end
