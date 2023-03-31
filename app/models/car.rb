f5987f4eb0a3c2bf0705989be5063934661d21c6
Ibunhabibu <Ibunhabib@gmail.com>
2023-03-31 23:40:25 +0300

    Define the image url

--- app/models/car.rb
+++ app/models/car.rb
@@ -8,7 +8,7 @@ class Car < ApplicationRecord
     # image.blob.service_url if image.attached?
     # Rails.application.routes.url_helpers.url_for(image) if image.attached?
     if image.attached?
-      cl_image_tag(image.key, width: 300, height: 300, crop: :fill)
+      cl_image_tag(self.image.key, width: 300, height: 300, crop: :fill)
     end
   end
 end
