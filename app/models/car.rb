496a2ced482f260e743ed973cbb958078fae688b
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-03 04:02:18 +0300

    Serialize the output

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,12 +6,12 @@ class Car < ApplicationRecord
   scope :recent, ->(max_limit = 8) { order('created_at').limit(max_limit) }
 
   def imageUrl
-    image.url if image.attached?
+    # image.url if image.attached?
     # Rails.application.routes.url_helpers.url_for(image) if image.attached?
-    # if image.attached?
-      # cl_image_tag(image.key, width: 300, height: 300, crop: :fill)
+    if image.attached?
+      cl_image_tag(self.image.key, width: 300, height: 300, crop: :fill)
       # Cloudinary::Utils.cloudinary_url(image)
-    # end
+    end
     
   end
 end
