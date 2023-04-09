e042aa3b6da1ae277b469638f260756b55f51dc1
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-09 17:56:31 +0300

    Redeploy the app

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,3 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+bundle exec rails db:create
+bundle exec rails db:migrate
