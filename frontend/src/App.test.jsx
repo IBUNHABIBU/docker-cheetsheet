0deb1b79b5a4ae206cd1de8c1a6b471edd0ea85c
IBUNHABIBU <ibunhabib@gmail.com>
2025-02-03 21:04:35 +0300

    test the app

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders button with text and handles click', () => {
+describe('App', () => {
   
 });
