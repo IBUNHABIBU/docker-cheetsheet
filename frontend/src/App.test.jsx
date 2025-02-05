1a99d5703a8fdccb3b315995dceec5b6a4005af9
IBUNHABIBU <ibunhabib@gmail.com>
2025-02-05 22:41:45 +0300

    Add test

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from '@testing-library/react';
-// import App from './App';
 import App from './App';
 import { expect } from 'vitest';
 
