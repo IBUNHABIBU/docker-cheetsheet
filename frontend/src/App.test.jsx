b8e658d348ab4acae6f02445170efbd111316eb3
IBUNHABIBU <ibunhabib@gmail.com>
2025-02-09 22:03:00 +0300

    Add another test

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -38,4 +38,13 @@ describe('App Component', () => {
     const countElement = screen.getByText(/count is 1/i)
     expect(countElement).toBeInTheDocument()
   })
+  test('Increment counnt when the button is clicked', () => {
+    render(<App />)
+    // find the button and click it
+    const buttonElement = screen.getByRole('button', { name: /count is 0/i })
+    expect(buttonElement).toBeInTheDocument();
+    fireEvent.click(buttonElement);
+    const countElement = screen.getByText(/count is 1/i)
+    expect(countElement).toBeInTheDocument()
+  })
 });
