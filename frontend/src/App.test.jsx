8104d0d52f65e9f4d5bb33ae5703514121a2c22c
IBUNHABIBU <ibunhabib@gmail.com>
2025-02-09 22:02:20 +0300

    Add another test

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -30,7 +30,6 @@ describe('App Component', () => {
   })
 
   test('Increment counnt when the button is clicked', () => {
-
     render(<App />)
     // find the button and click it
     const buttonElement = screen.getByRole('button', { name: /count is 0/i })
